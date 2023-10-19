e316a0608808f4ee7508f7fa89883e70b0be43e8
Maxwell <maxwell.wroblewsky@edu.sait.ca>
2023-10-19 16:05:06 -0600

    Changes 3

--- temp.py
+++ temp.py
@@ -1,10 +1,18 @@
-'''
-    Program to convert C to F
-'''
+'''    Program to convert C to F '''
 def celcius_to_fahrenheit(c):
     '''convers celcius to fahrenheit'''
     f = c * 9/5 + 32
     return f
+
+def fahrenheit_to_celcius(f):
+    '''convers celcius to fahrenheit'''
+    c = f -32 *5/9
+    return c
+
 c = 25
+f = 82
 f = celcius_to_fahrenheit(c)
 print(f"{c} degrees is {f} fahrenheit")
+
+c = fahrenheit_to_celcius(f)
+print(f"{f}degrees fahrenheit is {c} celcius")
